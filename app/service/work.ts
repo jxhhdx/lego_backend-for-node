2a01db07c33e072c7e4584132b3b982ef2f66039
gxuud <igaoxian@foxmail.com>
2023-02-28 16:24:40 +0800

    feat: 新增接口

--- app/service/work.ts
+++ app/service/work.ts
@@ -54,4 +54,31 @@ export default class WorkService extends Service {
     const { uuid } = res
     return `${H5BaseURL}/p/${id}-${uuid}`
   }
+  async copyWork(wid: number) {
+    const { ctx } = this
+    const copiedWork = await this.ctx.model.Work.findOne({ id: wid })
+    if (!copiedWork || !copiedWork.isPublic) {
+      throw new Error('can not be copied')
+    }
+    const uuid = nanoid(6)
+    const { content, title, desc, coverImg, id, copiedCount } = copiedWork
+    const { _id, username } = ctx.state.user
+    const newWork: WorkProps = {
+      user: _id,
+      author: username,
+      uuid,
+      coverImg,
+      copiedCount: 0,
+      status: 1,
+      title: `${title}-复制`,
+      desc,
+      content,
+      isTemplate: false
+    }
+    const res = await ctx.model.Work.create(newWork)
+    await ctx.model.Work.findOneAndUpdate({ id }, {
+      copiedCount: copiedCount + 1,
+    })
+    return res
+  }
 }
